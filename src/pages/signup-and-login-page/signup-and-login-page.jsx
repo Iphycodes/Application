fe28ce3a3c9b3db2bbe6aa97d9979c12e60e508e
Iphycodes <ifeanyiemmanuel585@gmail.com>
2022-05-04 15:58:21 -0700

    Added login

--- src/pages/signup-and-login-page/signup-and-login-page.jsx
+++ src/pages/signup-and-login-page/signup-and-login-page.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { MainContainer, PageBackground } from "./signup-and-login-page.styled";
 
+
 const SignUpAndSignIn = () => {
 
     
 
     return (
       <PageBackground>
-          <MainContainer>
-              
-          </MainContainer>
+        <Card style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
       </PageBackground>
     )
 }
